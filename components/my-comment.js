6ead48d06cb04ad4b9a235c35c27eb6eaee01d65
Eric <eric.eta.george@gmail.com>
2022-08-15 01:32:41 -0400

    Week6

--- components/my-comment.js
+++ components/my-comment.js
@@ -27,6 +27,7 @@ class MyComment extends HTMLElement {
         <h2>Comments</h2>
         <p>
             <ul id="todos"></ul>
+            <p id="demo-date"></p>
         </p>
     </div>
 </div>
@@ -35,11 +36,41 @@ class MyComment extends HTMLElement {
 <form>
 
     <!--         <input type="submit" value="Add Todo"> -->
-
+    <p>
+          photo:<img
+            src="https://picsum.photos/200/300?random=1"
+            alt="Smiley face"
+            width="100"
+            height="110"
+            style="vertical-align: middle"
+            id="photoId"
+            class="comment-class"
+          />
+        </p>
+        <p>
+        Photo:<input
+          class="comment-class"
+          id="imageInput"
+          name="photo"
+          type="text"
+          placeholder="image URL"
+        />
+      </p>
     Name: <input name="name" type="text"><br> Email: <input name="email" type="text"><br> Comment:
-    <input name="todo" type="text"><br>
+    <textarea name="todo" type="text"></textarea><br>
+    <p id="demo-id"></p>
+
+    <input
+          class="comment-class"
+          type="checkbox"
+          id="checkid"
+          name="checkid"
+          value="Agree"
+        />
+    <label for="checkid"> I agree to have the comment posted</label><br>
     <!-- <input type="submit" value="Add Comment"> -->
-    <button id="submit" onclick="submitAllComments()">Submit</button>
+    <!-- <button id="submit" onclick="submitAllComments()">Submit</button>-->
+    <button id="submit" onclick="myCheckFunction()">Submit</button> 
     <button id="clear-all" onclick="clearAllComments()">Clear All</button>
 
 </form>
@@ -59,6 +90,7 @@ const form = document.querySelector('form');
 const input = document.querySelector("[name='todo']");
 const inputEmail = document.querySelector("[name='email']");
 const inputName = document.querySelector("[name='name']");
+// const checkid = document.getElementById('checkid');
 
 const todoList = document.getElementById('todos');
 const emailList = document.getElementById('todos');
@@ -117,29 +149,69 @@ function addName(nameText) {
 }
 
 // Events
-form.onsubmit = (event) => {
-    event.preventDefault();
-    addTodo(input.value);
-    addEmail(inputEmail.value);
-    addName(inputName.value);
+// form.onsubmit = (event) => {
+//     event.preventDefault();
+//     let x = document.getElementById("checkid").checked;
+
+//     if (x == 'true') {
+//         // console.log.x;
+//         addName(inputName.value);
+//         addEmail(inputEmail.value);
+//         addTodo(input.value);
+//         displayDate();
+
+//     } else {
+//         document.getElementById("demo-id").innerHTML = `<h4>Please check box to submit comment</h4>`;
+//     }
+
+// addName(inputName.value);
+// addEmail(inputEmail.value);
+// addTodo(input.value);
+// displayDate();
+// }
+
+function myCheckFunction() {
+
+    let x = document.getElementById("checkid").checked;
+
+    if (x == true) {
+        // console.log.x;
+        addName(inputName.value);
+        addEmail(inputEmail.value);
+        addTodo(input.value);
+        displayDate();
+
+    } else {
+        document.getElementById("demo-id").innerHTML = `<h4>Please check box to submit comment</h4>`;
+    }
+
 }
 
+function displayDate() {
+    document.getElementById("demo-date").innerHTML = Date();
+}
 
 document.getElementById("clear-all").addEventListener("click", clearAllComments);
 // clear the array
 function clearAllComments() {
-    input.value = ''
-    inputEmail.value = ''
-    inputName.value = ''
-    todoList.innerHTML = "";
-    emailList.innerHTML = "";
-    nameList.innerHTML = "";
+    input.value = '';
+    inputEmail.value = '';
+    inputName.value = '';
+    todoList.innerHTML = '';
+    emailList.innerHTML = '';
+    nameList.innerHTML = '';
+
     // todoList.document.getElementById.clear('todos');
 
-    window.localStorage.clear();
-    localStorage.clear(existingTodos);
-    localStorage.clear(existingEmails);
-    localStorage.clear(existingNames);
+
+    localStorage.removeItem('todos');
+    localStorage.removeItem('emails');
+    localStorage.removeItem('names');
+    localStorage.clear();
+    document.getElementById("demo-date").innerHTML = ``;
+    // localStorage.setItem();
+    // localStorage.clear(existingEmails);
+    // localStorage.clear(existingNames);
 }
 
 
